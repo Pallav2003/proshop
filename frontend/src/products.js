32ed42740ddfb84b54c1a03daf81314fa18aa0f0
Choudhary <pachoudhary@galaxe.com>
2025-08-22 23:24:18 +0530

    Created Header, Footer, Cards with Temporary Data, Routing

---
+++ frontend/src/products.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        _id:'1',
+        name: 'Airpods',
+        image: '/images/airpods.jpg',
+        description: 'Bluetooth technology',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: '89.99',
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+]
+
+export default products;
