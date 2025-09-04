7459f1fe7366bf59d44abe0d7772a1d3ba42ee84
Choudhary <pachoudhary@galaxe.com>
2025-09-05 01:48:52 +0530

    admin can edit the product details along with uploading an image for product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,47 +1,71 @@
-import asyncHandler from "../middleware/asyncHandler.js"
+import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
 // @desc  Fetch all products
 // @route GET /api/products
 // @access Public
-const getProducts = asyncHandler(async(req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
 });
-
 
 // @desc  Fetch a product
 // @route GET /api/products/:id
 // @access Public
-const getProductById = asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id);
-    
-    if(product) {
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Resource not found');
-    }
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
 });
 
 // @desc  Create a Product
 // @route POST /api/products
 // @access Private/Admin
-const createProduct = asyncHandler(async(req, res) => {
-    const product = new Product({
-        name: 'Sample name',
-        price: 0,
-        user: req.user._id,
-        image: '/image/sample.jpg',
-        brand: 'Sample brand',
-        category: "Sample category",
-        countInStock: 0,
-        numReviews: 0,
-        description: "Sample description",
-    })
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
 
-    const createProduct = await product.save();
-    res.status(201).json(createProduct);
+  const createProduct = await product.save();
+  res.status(201).json(createProduct);
 });
 
-export {getProducts, getProductById, createProduct};
+// @desc  Edit a Product
+// @route PUT /api/products
+// @access Private/Admin
+const updateProduct = asyncHandler( async (req, res) => {
+  const {name, price, description, image, brand, category, countInStock} = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById, createProduct, updateProduct };
