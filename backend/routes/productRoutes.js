97804cbc959396ec2e6c38ff028265732b2886ba
Choudhary <pachoudhary@galaxe.com>
2025-09-06 19:05:18 +0530

    final commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,12 +12,12 @@ import {
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/top").get(getTopProducts);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
-router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
